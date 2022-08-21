db23eebe2a9a2a2dfed24fdb27921ae8461f2cea
Nightingale <youssefbahgat@live.com>
2022-08-21 23:43:14 +0200

    Nicely 69

--- Source.c
+++ Source.c
@@ -5,12 +5,6 @@ void main()
 	printf("enter number of name chars\n");
 	int x=0;
 	scanf("%d", &x);
-	fflush(stdin);
-	printf("enter name\n");
-	char* ptr = (char*) calloc(x, sizeof(char));
-	gets(ptr); fflush(stdin);
-	printf("name is: %s", ptr);
-	free(ptr);
 	return;
 }
 
